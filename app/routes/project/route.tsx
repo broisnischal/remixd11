53c1d574871d39381f027377921444ce49878915
broisnischal <neeswebservice@gmail.com>
2024-07-09 09:59:36 +0545

    changes

---
+++ app/routes/project/route.tsx
@@ -0,0 +1,237 @@
+export default function Page() {
+	return (
+		<div>
+			<h2>Projects for YEAR</h2>
+			<div>
+				<h1>RUST</h1>
+				<ul>
+					<li>
+						Build an SMTP server with mechanisms for detecting and filtering
+						spam.
+					</li>
+					<li>
+						Build an HTTP server with mechanisms for detecting and filtering
+						spam.
+					</li>
+					<li>
+						Implement a GZIP compression library for file compression and
+						decompression.
+					</li>
+					<li>
+						Create a vector database optimized for high-performance searches.
+					</li>
+					<li>Develop a SQL-like database that operates entirely in memory.</li>
+					<li>
+						Build a simple interpreted programming language with type inference
+						capabilities.
+					</li>
+					<li>
+						Create a library of concurrency primitives like channels and
+						mutexes.
+					</li>
+					<li>
+						Create a task queue system for distributing and executing tasks
+						across multiple nodes.
+					</li>
+					<li>
+						Develop a lightweight web framework optimized for performance and
+						security.
+					</li>
+					<li>
+						Implement a library for common cryptographic functions like
+						encryption, hashing, and digital signatures.
+					</li>
+					<li>
+						Create a server that provides image processing capabilities like
+						resizing and filtering via HTTP.
+					</li>
+				</ul>
+			</div>
+			<div>
+				<h1>GO</h1>
+				<ul>
+					<li>
+						Create a command-line utility to search for files based on various
+						criteria.
+					</li>
+					<li>
+						Build a TCP load balancer with health checks for backend servers to
+						ensure availability.
+					</li>
+					<li>
+						{' '}
+						<strong>Real-time Data Streaming Platform (Go)</strong>
+						Develop a platform for ingesting and processing real-time data
+						streams.
+					</li>
+					<li>
+						Develop a server for streaming video content to multiple clients.
+					</li>
+				</ul>
+			</div>
+
+			<div>
+				<h2>Typescript and Web</h2>
+				<ul>
+					<li>
+						Build a static site generator that converts Markdown files into a
+						static website.
+					</li>
+					<li>
+						Create a basic 2D game engine to handle graphics, input, and
+						physics.
+					</li>
+					<li>
+						Develop a library for client-side form validation with support for
+						custom rules.
+					</li>
+					<li>
+						Implement a task scheduler for running periodic tasks in web
+						applications.
+					</li>
+					<li>
+						Create a library for managing client-side data storage using
+						IndexedDB.
+					</li>
+					<li>
+						Develop a tool for visualizing graphs and networks in a web browser.
+					</li>
+				</ul>
+			</div>
+
+			<div>
+				<h2>Backend | Python</h2>
+				<ul>
+					<li>
+						Create a web framework similar to Express but with built-in support
+						for GraphQL APIs.
+					</li>
+					<li>
+						Develop a server that dynamically processes images on request.
+					</li>
+					<li>
+						Implement a system for sending real-time notifications to users via
+						WebSocket, and Use Kafka, or rabbit mq also qstack.
+					</li>
+				</ul>
+			</div>
+			<div>
+				<h2>ESP32</h2>
+				<ul>
+					<li>
+						Develop a web server that serves static files from a local
+						directory.
+					</li>
+					<li>
+						Realtime light control with react in the frontend and backend.
+					</li>
+				</ul>
+			</div>
+
+			<div>
+				<h2>Microservices</h2>
+				<ul>
+					<li>Multiple, service with kafka and redwood, communication.</li>
+				</ul>
+			</div>
+
+			<div>
+				<h2>C</h2>
+				<ul>
+					<li>Simple TCP/UDP Server and Client</li>
+					<li>
+						Implement a custom memory allocator that manages dynamic memory
+						allocation and deallocation.
+					</li>
+					<li>
+						<strong>Network Packet Sniffer</strong>
+						Create a tool to capture and analyze network packets on a specified
+						interface.
+						<small>
+							Network protocols, packet analysis, and low-level system access.
+						</small>
+					</li>
+					<li>
+						{' '}
+						SQLite-like Database Engine | Build a simple, file-based SQL
+						database engine similar to SQLite.
+					</li>
+					<li>
+						Create a file compression and decompression tool using a basic
+						compression algorithm. | Data compression algorithms, file handling,
+						and bit manipulation.
+					</li>
+				</ul>
+			</div>
+
+			<div>
+				<h2>Cpp</h2>
+				<ul>
+					<li>
+						Neural Network Library | Create a library for building and training
+						neural networks from scratch.
+					</li>
+					<li>
+						{' '}
+						Multithreaded Task Scheduler | Create a task scheduler that manages
+						and executes tasks concurrently using multiple threads.
+					</li>
+					<li>Build a video player with support for basic media controls.</li>
+				</ul>
+			</div>
+
+			<div>
+				<h2>Embedded Rust</h2>
+				<ul>
+					<li></li>
+				</ul>
+			</div>
+
+			<div>
+				<h2>Rxjs</h2>
+				<ul>
+					<li>Obserables, Subjects, and Operators | Create a library for</li>
+				</ul>
+			</div>
+
+			<div>
+				<h2>Framer Motion</h2>
+				<ul>
+					<li>50 Days of framer motion.</li>
+				</ul>
+			</div>
+
+			<div>
+				<h2>AI|ML</h2>
+				<ul>
+					<li>RAG implmentation</li>
+					<li>
+						Build and deploy a machine learning model to a web application for
+						predictions.
+					</li>
+					<li>
+						Build a dashboard to visualize data using libraries like Matplotlib
+						or Plotly. | React
+					</li>
+				</ul>
+			</div>
+
+			<div>
+				<h2>Auth Library</h2>
+			</div>
+
+			<div>
+				<h2>Algorithms</h2>
+				<p>
+					Sorting: QuickSort, MergeSort, BubbleSort. Searching: Binary Search,
+					Depth-First Search (DFS), Breadth-First Search (BFS). Dynamic
+					Programming: For optimization problems like Knapsack, Fibonacci. Graph
+					Algorithms: Dijkstra’s for shortest path, Kruskal’s for Minimum
+					Spanning Tree.
+				</p>
+			</div>
+		</div>
+	);
+}
+
+// ftp and smtp server

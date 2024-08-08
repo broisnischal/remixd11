734623ebe0332335f17663e8db52c3781ac53345
broisnischal <neeswebservice@gmail.com>
2024-08-08 20:49:00 +0545

    changes

--- app/routes/all-blogs[.json].ts
+++ app/routes/all-blogs[.json].ts
@@ -6,8 +6,9 @@ export async function loader({ context: { env } }: LoaderFunctionArgs) {
 	const posts = await getPosts();
 
 	return json(posts, {
+		// 'Cache-Control': `public, max-age=${60 * 60 * 24}`,
 		headers: {
-			'Cache-Control': `public, max-age=${60 * 60 * 24}`,
+			'Cache-Control': `public, s-maxage=60, stale-while-revalidate=${60 * 60}`,
 		},
 	});
 }

da9aa787e3e7b8b787fe5d7efb661635f08193d5
broisnischal <neeswebservice@gmail.com>
2024-08-15 00:48:45 +0545

    changes

--- app/routes/blog_._index.tsx
+++ app/routes/blog_._index.tsx
@@ -1,12 +1,43 @@
-import { json, MetaFunction } from '@remix-run/cloudflare';
+import { json, LoaderFunctionArgs, MetaFunction } from '@remix-run/cloudflare';
 import { useLoaderData } from '@remix-run/react';
+import { useState } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 import { getPosts } from '~/.server/posts';
 import Hr from '~/components/hr';
 import { Post } from '~/components/post';
 import { MetaCreator } from '~/utils/meta';
 
-export const loader = async () => json(await getPosts());
+export async function loader({ request }: LoaderFunctionArgs) {
+	const posts = await getPosts();
+
+	const url = new URL(request.url);
+	const search = url.searchParams.get('q');
+
+	if (search) {
+		return json({
+			posts: posts
+				.filter(post =>
+					post.frontmatter.title.toLowerCase().includes(search.toLowerCase()),
+				)
+				.sort((a, b) => {
+					if (!a.frontmatter.published) return 1;
+					if (!b.frontmatter.published) return -1;
+
+					const aDate = new Date(a.frontmatter.published);
+					const bDate = new Date(b.frontmatter.published);
+
+					if (aDate > bDate) return -1;
+					if (aDate < bDate) return 1;
+
+					return 0;
+				}),
+			search,
+		});
+	}
+
+	return json({ posts, search });
+}
 
 export const meta: MetaFunction<typeof loader> = ({
 	data,
@@ -23,7 +54,8 @@ export const meta: MetaFunction<typeof loader> = ({
 		others: [
 			{
 				name: 'keywords',
-				content: data && data.map(post => post.frontmatter.keywords).join(', '),
+				content:
+					data && data.posts.map(post => post.frontmatter.keywords).join(', '),
 			},
 			{
 				tagName: 'link',
@@ -40,7 +72,7 @@ export const meta: MetaFunction<typeof loader> = ({
 					'@context': 'https://schema.org',
 					'@type': 'WebPage',
 					name: 'Blogs by Nischal Dahal | broisnees ',
-					description: `List of blogs written by Nischal Dahal, ${data && data.length} posts, ${data && data.map(post => post.frontmatter.title).join(', ')}`,
+					description: `List of blogs written by Nischal Dahal, ${data && data.posts.length} posts, ${data && data.posts.map(post => post.frontmatter.title).join(', ')}`,
 					jobTitle: 'Full Stack Developer',
 					telephone: '+977 9741844523',
 					genderName: 'male',
@@ -48,7 +80,7 @@ export const meta: MetaFunction<typeof loader> = ({
 					address: 'Kathmandu, Nepal',
 					url: `${url.origin}${location.pathname}`,
 					mainEntity: [
-						data?.map(post => {
+						data?.posts.map(post => {
 							return {
 								'@type': 'Blog',
 								// '@type': 'Article',
@@ -72,12 +104,112 @@ export const meta: MetaFunction<typeof loader> = ({
 };
 
 export default function Component() {
-	const posts = useLoaderData<typeof loader>();
+	const { posts, search } = useLoaderData<typeof loader>();
+
+	const [allposts, setAllPosts] = useState(posts);
+
+	const nonRepetitiveTags = posts
+		.map(post => post.frontmatter.tags)
+		.flat()
+		.filter((tag, i, arr) => arr.indexOf(tag) === i)
+		.filter(tag => tag !== undefined);
+
+	const repetitiveTags = posts
+		.map(post => post.frontmatter.tags)
+		.flat()
+		.filter(tag => tag !== undefined)
+		.reduce(
+			(acc, tag) => {
+				const existingTag = acc.find(t => t.tag === tag);
+				if (existingTag) {
+					existingTag.count++;
+				} else {
+					acc.push({ tag, count: 1 });
+				}
+				return acc;
+			},
+			[] as { tag: string; count: number }[],
+		)
+		.filter(tagObj => tagObj.count > 1) // Filter only the repetitive tags
+		.map(tagObj => tagObj.tag); // Optionally, just return the tag names
+
+	const tags =
+		repetitiveTags.length > 10
+			? repetitiveTags.slice(0, 12)
+			: [
+					...repetitiveTags,
+					...nonRepetitiveTags
+						.filter(tag => !repetitiveTags.includes(tag))
+						.slice(0, 12 - repetitiveTags.length),
+				];
+
+	const tousetag = tags.map(tag => ({
+		tag,
+		active: false,
+	}));
+
+	const [activeTag, setActiveTag] = useState(tousetag);
+
+	const handleChange = ({ tag, active }: { tag: string; active: boolean }) => {
+		if (tag === '') return;
+
+		setActiveTag(
+			tousetag.map(t => ({
+				...t,
+				// t.active ||
+				active: t.tag === tag,
+			})),
+		);
+		const filteredPosts = posts.filter(
+			post =>
+				(post.frontmatter.tags && post.frontmatter.tags?.includes(tag)) ||
+				activeTag.every(t => t.tag === tag),
+		);
+
+		setAllPosts(filteredPosts);
+	};
 
 	return (
 		<div className="">
+			<div className="tags flex flex-wrap gap-2">
+				<input className="border" type="text" />
+				{activeTag &&
+					activeTag.map((tag, i) => (
+						<button
+							key={i}
+							className={twMerge(
+								'tag rounded-md border bg-secondary px-2 text-sm font-medium lowercase',
+								tag.active && 'bg-black text-white',
+							)}
+							onClick={() => {
+								handleChange({
+									tag: tag.tag,
+									active: false,
+								});
+
+								setActiveTag(
+									activeTag.map(t => ({
+										...t,
+										active: t.active || t.tag === tag.tag,
+									})),
+								);
+							}}
+						>
+							# {tag.tag}
+						</button>
+					))}
+				{/* <button
+					className={twMerge(
+						'tag rounded-md border bg-secondary px-2 text-sm font-medium lowercase',
+					)}
+					onClick={() => {}}
+				>
+					x
+				</button> */}
+			</div>
+			<Hr />
 			<ul className="space-y-14 ">
-				{posts.map((post, i) => (
+				{allposts.map((post, i) => (
 					<div key={i}>
 						<li key={i} className="">
 							<Post key={post.slug} {...post} />

3fe6d0f3a9657e84710d91deb0f6aa6d0216ca41
broisnischal <neeswebservice@gmail.com>
2024-09-07 15:58:54 +0545

    changes

--- app/routes/sponsor/route.tsx
+++ app/routes/sponsor/route.tsx
@@ -7,27 +7,33 @@ import { Button } from '~/components/ui/button';
 export default function Page() {
 	return (
 		<div className="flex flex-col gap-2">
-			<div className="flex flex-col items-start gap-4">
-				<img
-					src="https://avatars.githubusercontent.com/u/98168009?v=4"
-					className="w-[100px] rounded-md border bg-transparent shadow-sm saturate-50 hover:saturate-100 "
-					alt=""
-				/>
-				<div className="flex flex-col gap-2">
-					<p className="font-bricolage">
-						Hello, kind stranger who wants to help an open source developer ! I
-						maintain and create open source projects. I am very passionate about
-						what I do and I want to continue to do so. If you find my content
-						useful, please consider sponsoring me.
-					</p>
-					<p>
-						Your sponsorship will help me to continue to create and share
-						content that you or others can use for free.
-					</p>
-				</div>
+			<h1 className="font-bricolage text-4xl font-bold">Sponsor</h1>
+			<p className="">
+				Your sponsorship will help me to continue to create and share content
+				that you or others can use for free.
+			</p>
+
+			{/* <img
+				src="https://ko-fi.com/streamalerts/goaloverlay/sa_390dc74c-6a48-4c6f-9098-036e86e234f7.png"
+				alt=""
+			/> */}
+			<div className="div bg-white pt-12">
+				<iframe
+					id="kofiframe"
+					className="m-auto w-full"
+					src="https://ko-fi.com/nischaldahal/?hidefeed=true&widget=true&embed=true&preview=true"
+					style={
+						{
+							// borderRadius: '0.5rem',
+							// background: 'transparent',
+						}
+					}
+					height={712}
+					title="nischaldahal"
+				></iframe>
 			</div>
 			<br />
-			<Link
+			{/* <Link
 				target="_blank"
 				// className="self-center"
 				to={'https://ko-fi.com/nischaldahal'}
@@ -35,7 +41,9 @@ export default function Page() {
 				<Button variant="outline" size={'lg'} className="flex gap-2">
 					<SiGithubsponsors /> <span>Sponsor</span>
 				</Button>
-			</Link>
+			</Link> */}
+
+			<br />
 		</div>
 	);
 }
